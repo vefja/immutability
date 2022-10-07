292056603f5b34ef6d329a94af0259d54296f9cc
tekq <81379378+tekq@users.noreply.github.com>
2022-10-07 21:54:13 +0300

    add main.rs with the new immutability API

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,49 @@
+use nix::unistd::getuid;
 use std::env;
-mod api;
+mod imut_api;
 
 fn main() {
-    if api::getmode() {
-        println!("System is in immutable mode");
-    } else {
-        println!("System is not in immutable mode");
-    }
-
     let args: Vec<String> = env::args().collect();
 
-    if args[1] == "enable" {
-        println!("Enabling immutable mode");
-        api::enterro();
-    } else if args == ["immutable", "disable"] {
-        println!("Disabling immutable mode");
-        api::enterrw();
+    if args.len() == 1 {
+        println!("Usage: {} <command>", args[0]);
+        return;
     }
+
+    // TODO: Don't let this be activated if args are 0
+    if args[1] == "enable" {
+        if getuid().to_string() != "0" {
+            println!("You must be root to modify the filesystem");
+            return;
+        }
+
+        if !imut_api::getmode() {
+            imut_api::enterro();
+            println!("Filesystem is now immutable");
+        } else {
+            println!("System is already immutable");
+        }
+    } else if args[1] == "disable" {
+        if getuid().to_string() != "0" {
+            println!("You must be root to modify the filesystem");
+            return;
+        }
+
+        if imut_api::getmode() {
+            imut_api::enterrw();
+            println!("Filesystem is now mutable");
+        } else {
+            println!("System is already mutable");
+        }
+
+    } else if args[1] == "check" {
+        if imut_api::getmode() {
+            println!("System is in immutable mode");
+        } else {
+            println!("System is not in immutable mode");
+        }
+    } else {
+        println!("Invalid argument");
+    }
+
 }
